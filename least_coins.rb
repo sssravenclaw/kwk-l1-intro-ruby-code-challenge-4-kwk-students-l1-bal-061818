7fea8add58a7e4a3e8438335cb53837b8045b388
Siobhan <yuriamisaki18@gmail.com>
2018-06-21 17:21:26 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,12 @@
 #write out your code here
 
-def least_coins(cents)
-
-#Code your answer here!
-
+coin_types = {}
+def least_coins (cents)
+  coin_types = {
+    "quarters"=> 0, "dimes", => 0, "nickles", => 0, "pennies", =>
+  }
+quarters = cents/25
+if quarters == 0
+  coin_types["quarters"] = 0
+else quarters == 25
 end
